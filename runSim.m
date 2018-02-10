847694aa4d43739e7931219e2dcb9e38270f16f8
vpan14 <vpan14@gmail.com>
2018-02-10 13:20:00 -0500

    cleaned up simulation

--- runSim.m
+++ runSim.m
@@ -1,9 +1,18 @@
 function runSim()
+    %params
+    carLength = 0.4;
+    noise = 0;
+    position_theta = 0;
+    position_x = 0;
+    position_y = 0;
 
-%carLength, noise, position_theta, position_x, position_y, velocity, steering_angle
-  
-  [carLength, noise, position_theta, position_x, position_y, velocity, steering_angle] = acerkmannSim([0.4, 0, 0, 0, 0, 2, (pi / 4)]);
-  for i = 0:100
-    [carLength, noise, position_theta, position_x, position_y, velocity, steering_angle] = acerkmannSim([carLength, noise, position_theta, position_x, position_y, velocity, steering_angle]);
-  endfor
-endfunction
+    %velocity
+    velocity = 2; %m/s rear
+    steering_angle = pi/4; %rads
+    dt = 0.05;
+
+    for t = 0:100
+        [carLength, noise, position_theta, position_x, position_y, velocity, steering_angle] = ackermannSim(carLength, noise, position_theta, position_x, position_y, velocity, steering_angle, dt);
+        drawnow;
+    end
+end
