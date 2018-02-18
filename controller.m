7c17aa98d89dbc91023aa177f119145d36c225fb
vpan14 <vpan14@gmail.com>
2018-02-18 14:19:40 -0500

    added comments to controller

--- controller.m
+++ controller.m
@@ -12,37 +12,40 @@ function [T, phi, integral] = controller(dt, integral, est_state, desired_state)
 %     radius = carLength/sin(steering_angle);
 %     a_centripetal = velocity*velocity/radius;
     
+    %parameters
     carLength = 0.4;
     mass = 4;
     min_turning_rad = 1;
     min_thrust = -20;
     max_thrust = 20;
     
-    theta = est_state(3);
-    rotation_matrix = [cos(theta), -sin(theta);
+    theta = est_state(3); %angle between body frame and global axes
+    rotation_matrix = [cos(theta), -sin(theta); 
                         sin(theta), cos(theta)];
     
     error = transpose(rotation_matrix) * ([desired_state(1); desired_state(2)] - [est_state(1); est_state(2)]);
 
     error_dot = [desired_state(4); desired_state(5)] - [est_state(4); est_state(5)];
     
+    %gain coefficients
     K_P = [0.1, 0; 0, 0.1];
     K_D = [0.1, 0; 0, 0.1];
     K_i = [0.1, 0; 0, 0.1];
 
-    integral = integral + error * dt;
+    integral = integral + error * dt; %updates integral term
     
     output = [desired_state(6); desired_state(7)] + K_P * error + K_i * integral + K_D * error_dot
     
     radius = 0;
     
+    %if centripetal acceleration is 0
     if(output(2) == 0)
         radius = 1e12
     else
         radius = est_state(4)^2/output(2)
     end
     
-    T = mass * output(1);
+    T = mass * output(1); %thrust
     
     %constrain
     if(abs(radius) < min_turning_rad)
@@ -55,5 +58,5 @@ function [T, phi, integral] = controller(dt, integral, est_state, desired_state)
         T = max_thrust;
     end
     
-    phi = atan(carLength/radius);
+    phi = atan(carLength/radius); %steering angle
 end
