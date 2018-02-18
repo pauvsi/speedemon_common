a858fc6672b6572d120146fa31664af319dce14f
vpan14 <vpan14@gmail.com>
2018-02-18 13:51:38 -0500

    finished controller

--- controller.m
+++ controller.m
@@ -2,36 +2,58 @@
 %acceleration (forward acceleration) and steering angle
 
 %test
-sum_error_dt = [0; 0];
-est_state = [0; 0; 1; 1; 1; 1];
-% carLength = 0.4;
-steering_angle = pi/5;
-velocity = 2;
-desired_state = [.2; .3; 1.5; .6; 1; 1];
+% sum_error_dt = [0; 0];
+% est_state = [0; 0; 0; 1; 1; 1; 1]; %x, y, theta(body frame to global frame), bdx, bdy, bax, bay
+% desired_state = [0; 0; 0; 1; 1; 1; 1]; %x, y, theta(body frame to global frame), bdx, bdy, bax, bay
+% 
+% [T, phi, sum_error_dt] = controlle(.05, sum_error_dt, est_state, desired_state)
 
-[T, phi] = controlle(.05, sum_error_dt, est_state, steering_angle, desired_state)
-
-function [T, phi, sum_error_dt] = controlle(dt, sum_error_dt, est_state, steering_angle, desired_state)
+function [T, phi, integral] = controller(dt, integral, est_state, desired_state)
 %     radius = carLength/sin(steering_angle);
 %     a_centripetal = velocity*velocity/radius;
     
-    rotation_matrix = [cos(steering_angle), -sin(steering_angle);
-                        sin(steering_angle), cos(steering_angle)];
+    carLength = 0.4;
+    mass = 4;
+    min_turning_rad = 1;
+    min_thrust = -20;
+    max_thrust = 20;
     
-    error = rotation_matrix * ([desired_state(1); desired_state(2)] - [est_state(1); est_state(2)]);
-
-%     est_velocity = [velocity * sin(steering_angle); velocity * cos(steering_angle)];
-
-    error_dot = [desired_state(3); desired_state(4)] - [est_state(3); est_state(4)];
+    theta = est_state(3);
+    rotation_matrix = [cos(theta), -sin(theta);
+                        sin(theta), cos(theta)];
     
-    K_P = [1, 1; 1, 1];
-    K_D = [1, 0; 0, 1];
-    K_i = [1, 0; 0, 1];
+    error = transpose(rotation_matrix) * ([desired_state(1); desired_state(2)] - [est_state(1); est_state(2)]);
 
-    sum_error_dt = sum_error_dt + error * dt;
+    error_dot = [desired_state(4); desired_state(5)] - [est_state(4); est_state(5)];
+    
+    K_P = [0.1, 0; 0, 0.1];
+    K_D = [0.1, 0; 0, 0.1];
+    K_i = [0.1, 0; 0, 0.1];
 
-    output = [desired_state(5); desired_state(6)] + K_P * error + K_i * sum_error_dt + K_D * error_dot;
-
-    T = output(1);
-    phi = output(2);
+    integral = integral + error * dt;
+    
+    output = [desired_state(6); desired_state(7)] + K_P * error + K_i * integral + K_D * error_dot
+    
+    radius = 0;
+    
+    if(output(2) == 0)
+        radius = 1e12
+    else
+        radius = est_state(4)^2/output(2)
+    end
+    
+    T = mass * output(1);
+    
+    %constrain
+    if(abs(radius) < min_turning_rad)
+        radius = min_turning_rad;
+    end
+    
+    if(T < min_thrust)
+        T = min_thrust;
+    elseif(T > max_thrust)
+        T = max_thrust;
+    end
+    
+    phi = atan(carLength/radius);
 end
