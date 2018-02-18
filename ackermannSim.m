6e24132ede6a453f144e01a78488a1ee7af0ed21
vpasuma <vpasuma@purdue.edu>
2018-02-18 15:06:07 -0500

    ackerman fix

--- ackermannSim.m
+++ ackermannSim.m
@@ -1,23 +1,31 @@
-function [carLength, noise, position_theta, position_x, position_y, velocity, steering_angle] = ackermannSim(carLength, noise, position_theta, position_x, position_y, velocity, steering_angle, delta_time)
-
-    %angle
-    delta_theta = (velocity/carLength) * tan(steering_angle);
-
-    %odometry
-    delta_x = velocity * cos(position_theta + ((delta_theta * delta_time) / 2.0));
-    delta_y = velocity * sin(position_theta + ((delta_theta * delta_time) / 2.0));
-
-    %pose estimate update
-    position_x = position_x + delta_x * delta_time;
-    position_y = position_y + delta_y * delta_time;
-    position_theta = position_theta + delta_theta * delta_time;
-
-    %actual vector
-    ackermannVector = [position_x, position_y, cos(position_theta), sin(position_theta)];
-
-    quiver(position_x, position_y, cos(position_theta), sin(position_theta));
-    xlim([-10 10]);
-    ylim([-10 10]);
+function [globalState] = ackermannSim(desiredControlVector, mass, carLength, globalState, dt)
+    
+    %globalState = [x, y, dx, dy, T, phi) 
+    %desiredControlVector = [T, phi]
+    globalState(5) = globalState(5) + 0.98 * (desiredControlVector(1) - globalState(5)); %T, x, T
+    globalState(6) = globalState(6) + 0.5 * (desiredControlVector(2) - globalState(6)); %phi, Y, phi
+    
+    theta = atan2(globalState(4), globalState(3)); % intertial dy, dx
+    R = [cos(theta) , -sin(theta); sin(theta) , cos(theta)]; %rotation matrix from inertial coordinate frame to body coordinate frame
+    
+    r = carLength / tan(globalState(6); %turn radius (denom is phi)
+    
+    if (abs(r) < 1.1)
+      r = 1.1;
+    end
+    
+    if (globalState(5) < -20)
+      globalState(5) = -20;
+    end
+    
+    if (globalState(5) > 20)
+      globalState(5) = 20;
+    end
+      
+    intertialAcceleration = R([globalState(5) / mass; 0] + [0 ; globalState(3)^2 / r);
+    
+    globalState(1:2, 1) = globalState(1:2, 1) + dt * globalState(3:4, 1) + 0.5 * dt * dt * inertialAcceleration;
+    globalState(3:4, 1) = globalState(3:4, 1) + intertialAcceleration * dt;
   
 end
   
--- runSim.m
+++ runSim.m
@@ -5,14 +5,21 @@ function runSim()
     position_theta = 0;
     position_x = 0;
     position_y = 0;
+    mass = 50;
+    T = 1;
+    state = [position_x,position_y];
+    
 
     %velocity
-    velocity = 2; %m/s rear
+    velocity = -2; %m/s rear
     steering_angle = pi/4; %rads
     dt = 0.05;
 
     for t = 0:100
-        [carLength, noise, position_theta, position_x, position_y, velocity, steering_angle] = ackermannSim(carLength, noise, position_theta, position_x, position_y, velocity, steering_angle, dt);
+        [mass, carLength, noise, position_theta, state, velocity, steering_angle] = ackermannSim(carLength, mass, noise, position_theta, state, velocity, steering_angle, dt);
+        
+        xlim([-10 10]);
+        ylim([-10 10]);
         drawnow;
     end
 end
